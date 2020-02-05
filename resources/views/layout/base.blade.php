22a573b61ad4c79d61568a78c26a1a7652b84b2e
baseanalyzer <dbasedown211@gmail.com>
2020-02-05 20:52:24 +0700

    fixed navbar

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -12,25 +12,40 @@
   </head>
   <body>
     {{-- header --}}
-	<nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
-		<a class="navbar-brand" href="#">Navbar</a>
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-		  <span class="navbar-toggler-icon"></span>
-		</button>
-		<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-		  <div class="navbar-nav">
-			<a class="nav-item nav-link" href="{{ url('/') }}">Home</a>
-			<a class="nav-item nav-link" href="{{ url('/about') }}">About</a>
-			<a class="nav-item nav-link" href="{{ url('/barang') }}">Product</a>
-			<a class="nav-item nav-link" href="#">News</a>
-			<a class="nav-item nav-link" href="#">Services</a>
-		  </div>
-		</div>
-    <form class="form-inline">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </nav>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+      <a class="navbar-brand" href="#">
+        <img src="/docs/4.4/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+        Amptron
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <form class="form-inline my-2 my-lg-0 ml-5 mr-5">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+        <ul class="navbar-nav navbar-right ml-5 mr-auto">
+          <a class="nav-item nav-link pr-4" href="{{ url('/') }}">Home</a>
+          <a class="nav-item nav-link pr-4" href="{{ url('/about') }}">About</a>
+          <a class="nav-item nav-link pr-4" href="{{ url('/barang') }}">Product</a>
+          <a class="nav-item nav-link pr-4" href="#">News</a>
+          <a class="nav-item nav-link pr-4" href="#">Services</a>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Dropdown
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
     {{-- end header --}}
 
 

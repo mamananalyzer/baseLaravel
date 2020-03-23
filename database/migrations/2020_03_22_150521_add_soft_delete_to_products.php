9c5ada35e1cfbddc248c5244ecc228ae6a7b7654
baseanalyzer <dbasedown211@gmail.com>
2020-03-23 12:42:38 +0700

    asddd

--- database/migrations/2020_03_22_150521_add_soft_delete_to_products.php
+++ database/migrations/2020_03_22_150521_add_soft_delete_to_products.php
@@ -14,7 +14,7 @@ class AddSoftDeleteToProducts extends Migration
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->softDeletes();
+            // $table->softDeletes();
         });
     }
 
